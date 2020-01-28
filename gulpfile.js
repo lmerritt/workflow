96b12cd2c14ddc06a70faa3790eaf131591fd550
Lloyd Merritt <lloyd@merrittconsulting.com>
2020-01-28 16:38:33 -0600

    remove usedcss from gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,151 +1,147 @@
-const { src, dest, watch, series, parallel } = require('gulp'),
-    autoprefixer = require('autoprefixer'),
-    postcss = require('gulp-postcss'),
-    cssnano = require('cssnano'),
-    htmlmin = require('gulp-htmlmin'),
-    imagemin = require('gulp-imagemin'),
-    newer = require('gulp-newer'),
-    sass = require('gulp-sass'),
-    size = require('gulp-size'),
-    assets = require('postcss-assets'),
-    postcssPresetEnv = require('postcss-preset-env'),
-    uglify = require('gulp-uglify'),
-    usedcss = require('usedcss'),
-    babel = require('gulp-babel'),
-    rename = require('gulp-rename'),
-    browsersync = require('browser-sync').create();
-
-
+const { src, dest, watch, series, parallel } = require("gulp"),
+  autoprefixer = require("autoprefixer"),
+  postcss = require("gulp-postcss"),
+  cssnano = require("cssnano"),
+  htmlmin = require("gulp-htmlmin"),
+  imagemin = require("gulp-imagemin"),
+  newer = require("gulp-newer"),
+  sass = require("gulp-sass"),
+  size = require("gulp-size"),
+  assets = require("postcss-assets"),
+  postcssPresetEnv = require("postcss-preset-env"),
+  uglify = require("gulp-uglify"),
+  babel = require("gulp-babel"),
+  rename = require("gulp-rename"),
+  browsersync = require("browser-sync").create();
 
 /**************** html task ****************/
 const htmlConfig = {
-    src: 'dev/**/*.html',
-    dest: 'app/'
+  src: "dev/**/*.html",
+  dest: "app/"
 };
 
 function html() {
-    return src(htmlConfig.src)
-        .pipe(htmlmin({
-            minifyJS: true,
-            collapseWhitespace: true,
-            removeComments: true
-        }))
-        .pipe(dest(htmlConfig.dest));
+  return src(htmlConfig.src)
+    .pipe(
+      htmlmin({
+        minifyJS: true,
+        collapseWhitespace: true,
+        removeComments: true
+      })
+    )
+    .pipe(dest(htmlConfig.dest));
 }
 
 exports.html = html;
 
-
 /**************** js task ****************/
 const jsConfig = {
-    src: 'dev/js/**/*.js',
-    dest: 'app/js/'
+  src: "dev/js/**/*.js",
+  dest: "app/js/"
 };
 
 function js() {
-    return src([
-        jsConfig.src
-        //,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
-    ])
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(uglify())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(dest(jsConfig.dest));
+  return src([
+    jsConfig.src
+    //,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
+  ])
+    .pipe(
+      babel({
+        presets: ["@babel/env"]
+      })
+    )
+    .pipe(uglify())
+    .pipe(
+      rename({
+        suffix: ".min"
+      })
+    )
+    .pipe(dest(jsConfig.dest));
 }
 
 exports.js = js;
 
-
 /**************** image task ****************/
 const imgConfig = {
-    src: 'dev/images/**/*',
-    dest: 'app/images/'
+  src: "dev/images/**/*",
+  dest: "app/images/"
 };
 
 function images() {
-    return src(imgConfig.src)
-        .pipe(newer(imgConfig.dest))
-        .pipe(imagemin([
-            //options
-            imagemin.svgo({
-                plugins: [
-                    { removeViewBox: true },
-                    { cleanupIDs: false }
-                ]
-            })
-        ]))
-        .pipe(size({ showFiles: true }))
-        .pipe(dest(imgConfig.dest));
+  return src(imgConfig.src)
+    .pipe(newer(imgConfig.dest))
+    .pipe(
+      imagemin([
+        //options
+        imagemin.svgo({
+          plugins: [{ removeViewBox: true }, { cleanupIDs: false }]
+        })
+      ])
+    )
+    .pipe(size({ showFiles: true }))
+    .pipe(dest(imgConfig.dest));
 }
 
 exports.images = images;
 
-
 /**************** css task ****************/
 const scssConfig = {
-    src: 'dev/css/**/*.css',
-    watch: 'dev/css/**/*',
-    dest: 'app/css/',
-    sassOpts: {
-        imagePath: '/images/',
-        precision: 3,
-        errLogToConsole: true
-    },
+  src: "dev/css/**/*.css",
+  watch: "dev/css/**/*",
+  dest: "app/css/",
+  sassOpts: {
+    imagePath: "/images/",
+    precision: 3,
+    errLogToConsole: true
+  },
 
-    postCSS: [
-        usedcss({
-            html: [htmlConfig.src],
-            ignore: ['.error']
-        }),
-        postcssPresetEnv(/* pluginOptions */),
-        assets({
-            loadPaths: ['images/'],
-            basePath: 'app/'
-        }),
-        autoprefixer(),
-        cssnano()
-    ]
+  postCSS: [
+    postcssPresetEnv(/* pluginOptions */),
+    assets({
+      loadPaths: ["images/"],
+      basePath: "app/"
+    }),
+    autoprefixer(),
+    cssnano()
+  ]
 };
 
 function css() {
-    return src(scssConfig.src)
-        .pipe(sass(scssConfig.sassOpts).on('error', sass.logError))
-        .pipe(postcss(scssConfig.postCSS))
-        .pipe(size({ showFiles: true }))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(dest(scssConfig.dest))
-        .pipe(browsersync.reload({ stream: true }));
+  return src(scssConfig.src)
+    .pipe(sass(scssConfig.sassOpts).on("error", sass.logError))
+    .pipe(postcss(scssConfig.postCSS))
+    .pipe(size({ showFiles: true }))
+    .pipe(
+      rename({
+        suffix: ".min"
+      })
+    )
+    .pipe(dest(scssConfig.dest))
+    .pipe(browsersync.reload({ stream: true }));
 }
 
 exports.css = series(images, css);
 
-
 /**************** server task ****************/
 function serve(done) {
-    browsersync.init({
-        server: 'app',
-        browser: 'firefox'
-    });
-    done();
+  browsersync.init({
+    server: "app",
+    browser: "firefox"
+  });
+  done();
 }
 
 /**************** watch task ****************/
 function watchTask(done) {
-    watch(imgConfig.src, images);
+  watch(imgConfig.src, images);
 
-    watch(scssConfig.watch, css);
+  watch(scssConfig.watch, css);
 
-    watch(htmlConfig.src, html).on('change', browsersync.reload);
+  watch(htmlConfig.src, html).on("change", browsersync.reload);
 
-    watch(jsConfig.src, js).on('change', browsersync.reload);
+  watch(jsConfig.src, js).on("change", browsersync.reload);
 
-    done();
+  done();
 }
 
 /**************** default task ****************/
